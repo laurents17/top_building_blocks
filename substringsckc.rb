e78339f25f4064a587c358ff31d18daa1fbf2d42
Christina Clark <divackc@gmail.com>
2018-10-01 13:47:01 +0000

    updated

--- substringsckc.rb
+++ substringsckc.rb
@@ -1,3 +1,4 @@
+#def main
 def substrings(word, dictionary)
   word = word.downcase.split(" ") 
   word_counter = Hash.new(0)
@@ -6,3 +7,5 @@ def substrings(word, dictionary)
 end
 
 substrings("Drunk in love.  Single ladies. Dance for you. Resentment. Mine. I am Sasha Fierce!", ["drunk","in","love","i","am","sasha","fierce","single","ladies","dance","for","you","resentment","mine"])
+
+#main if __FILE__ == $PROGRAM_NAME
