817c4271cb9f5da31f15c757d677e92f3c620848
Christina Clark <divackc@gmail.com>
2018-10-01 13:35:14 +0000

    project3 ckc

---
+++ substringsckc.rb
@@ -0,0 +1,8 @@
+def substrings(word, dictionary)
+  word = word.downcase.split(" ") 
+  word_counter = Hash.new(0)
+  dictionary.each {|dictionary_word| word.each {|words_input|word_counter[dictionary_word] += 1 if words_input.include?(dictionary_word)}}
+  puts word_counter
+end
+
+substrings("Drunk in love.  Single ladies. Dance for you. Resentment. Mine. I am Sasha Fierce!", ["drunk","in","love","i","am","sasha","fierce","single","ladies","dance","for","you","resentment","mine"])
